f89cc4ea393b833c4cd46ac58cd8d8d8f56ce15e
Samson Cheung <samsonc89@yahoo.com>
2023-12-27 09:27:48 -0800

    Create index file to export multiple images

---
+++ src/images/index.js
@@ -0,0 +1,8 @@
+export { default as about1 } from "./about1.jpg";
+export { default as about2 } from "./about2.jpg";
+export { default as about3 } from "./about3.jpg";
+export { default as about4 } from "./about4.jpg";
+export { default as about5 } from "./about5.jpg";
+export { default as icon1 } from "./indexIcon1.svg";
+export { default as icon2 } from "./indexIcon2.svg";
+export { default as icon3 } from "./indexIcon3.svg";
