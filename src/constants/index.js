681c1739946e59d4c7efdb82e63808266b275297
Samson Cheung <samsonc89@yahoo.com>
2024-01-04 10:05:12 -0800

    Add constant folder for easier export

---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+export const indexHero = {
+  caption: "Power Up Your Business",
+  altText: "Hero image",
+  imagePath: "/src/images/homeHero.jpg",
+  title: "Illuminate Smarter and Save Bigger",
+  text: `Your unique energy solution begins with a consultation. See how we’ve helped thousands of businesses discover the brilliance of customized energy efficiency—schedule your consultation now.`,
+};
+
+export const aboutHero = {};
