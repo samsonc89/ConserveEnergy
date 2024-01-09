a4dd3f6c7276d2c42d7ba7673a2f03692cef458d
Samson Cheung <samsonc89@yahoo.com>
2024-01-09 10:02:15 -0800

    Add tailwind colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        green: "#247B46",
+        "caption-color": "#E06127",
+        "title-color": "#111211",
+        "body-color": "#3A3A3A",
+        bone: "#F9F6EE",
+        gray: "#9c9c9c",
+        white: "#FFFFFF",
+      },
+    },
+  },
+  plugins: [],
+};
