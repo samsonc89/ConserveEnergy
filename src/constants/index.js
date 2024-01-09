f7cb87a02cac59a495a001640127e12920a0a9af
Samson Cheung <samsonc89@yahoo.com>
2024-01-08 16:36:41 -0800

    Add text for services hero

--- src/constants/index.js
+++ src/constants/index.js
@@ -7,3 +7,11 @@ export const indexHero = {
 };
 
 export const aboutHero = {};
+
+export const serviceHero = {
+  caption: "Turnkey solutions",
+  altText: "Hero image",
+  imagePath: "/src/images/homeHero.jpg",
+  title: "Top-notch services",
+  text: `Vorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.`,
+};
