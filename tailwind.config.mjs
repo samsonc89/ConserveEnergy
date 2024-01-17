4139c3397f7efb2b85ae4d7689b3c315ca9b0398
Caleb Tseng <calebtseng07@gmail.com>
2024-01-17 09:14:36 -0800

    Adding files

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-    "./node_modules/flowbite/**/*.js",
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"
   ],
   theme: {
     extend: {
+      boxShadow: {
+        '3xl': '0 35px 60px 30px rgba(0, 0, 0, 0.3)',
+      },
       colors: {
         green: "#247B46",
         "caption-color": "#E06127",
@@ -17,5 +19,4 @@ export default {
       },
     },
   },
-  plugins: [require("flowbite/plugin")],
 };
