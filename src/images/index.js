a47918afdf69d4346d5b13f7c1c0d7d58c135ad6
Caleb Tseng <calebtseng07@gmail.com>
2024-01-20 04:52:51 -0800

    Create company component with logo images

--- src/images/index.js
+++ src/images/index.js
@@ -3,25 +3,42 @@ export { default as about2 } from "./about2.jpg";
 export { default as about3 } from "./about3.jpg";
 export { default as about4 } from "./about4.jpg";
 export { default as about5 } from "./about5.jpg";
+
 export { default as icon1 } from "./indexIcon1.svg";
 export { default as icon2 } from "./indexIcon2.svg";
 export { default as icon3 } from "./indexIcon3.svg";
+
 export { default as service1 } from "./service1.jpeg";
 export { default as service2 } from "./service2.jpeg";
 export { default as service3 } from "./service3.jpeg";
 export { default as service4 } from "./service4.jpeg";
 export { default as service5 } from "./service5.jpeg";
+
 export { default as homeHero } from "./homeHero.jpg";
 export { default as aboutHero } from "./aboutHero.png";
 export { default as whyUs } from "./whyUs.jpeg";
 export { default as map } from "./map.png";
 export { default as contact } from "./contact.jpeg";
+
 export { default as industrial1 } from "./industrial1.jpeg";
 export { default as industrial2 } from "./industrial2.jpeg";
 export { default as industrial3 } from "./industrial3.jpeg";
+
 export { default as convenience1 } from "./convenience1.jpeg";
 export { default as convenience2 } from "./convenience2.jpeg";
 export { default as convenience3 } from "./convenience3.jpeg";
+
 export { default as grocery1 } from "./grocery1.jpeg";
 export { default as grocery2 } from "./grocery2.jpeg";
 export { default as grocery3 } from "./grocery3.jpeg";
+
+export { default as company1 } from "./company1.jpeg";
+export { default as company2 } from "./company2.jpeg";
+export { default as company3 } from "./company3.jpeg";
+export { default as company4 } from "./company4.jpeg";
+export { default as company5 } from "./company5.jpeg";
+export { default as company6 } from "./company6.jpeg";
+export { default as company7 } from "./company7.jpeg";
+export { default as company8 } from "./company8.jpeg";
+export { default as company9 } from "./company9.jpeg";
+export { default as company10 } from "./company10.jpeg";
