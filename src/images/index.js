51fcac51d50ebae2e2f2047aa2d474a2161d2368
Caleb Tseng <calebtseng07@gmail.com>
2024-02-01 04:49:38 -0800

    Start utility incentives section

--- src/images/index.js
+++ src/images/index.js
@@ -47,3 +47,4 @@ export { default as pthp } from "./pthp.jpg";
 export { default as solar } from "./solar.jpg";
 
 export { default as aboutBg } from "./aboutBg.webp";
+export { default as utilityGraph } from "./utilityGraph.png";
